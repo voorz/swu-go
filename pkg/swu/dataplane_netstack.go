71f81ea53aef8881d15f9137b9c81e4d419d2c6e
voorz <voorz@users.noreply.github.com>
2026-09-18 00:26:01 +0800

    chore: 注释 netstack ESP 出站/入站 DEBUG 日志
    - dataplane_netstack: 注释 ESP 出站已发送/入站收到/入站已解密 三个高频 DEBUG 日志

--- pkg/swu/dataplane_netstack.go
+++ pkg/swu/dataplane_netstack.go
@@ -119,13 +119,13 @@ func (s *Session) startNetstackDataPlaneLoop() {
 					continue
 				}
 				espSendCount++
-				s.Logger.Debug(s.pfx("netstack ESP 出站已发送"),
-					logger.Uint64("txCount", txCount),
-					logger.Uint64("espSendCount", espSendCount),
-					logger.String("dstIP", dstIP),
-					logger.Int("proto", int(proto)),
-					logger.Int("innerLen", len(packet)),
-					logger.Int("espLen", len(espPacket)))
+				// s.Logger.Debug(s.pfx("netstack ESP 出站已发送"),
+				// 	logger.Uint64("txCount", txCount),
+				// 	logger.Uint64("espSendCount", espSendCount),
+				// 	logger.String("dstIP", dstIP),
+				// 	logger.Int("proto", int(proto)),
+				// 	logger.Int("innerLen", len(packet)),
+				// 	logger.Int("espLen", len(espPacket)))
 			}
 		}
 	}()
@@ -148,10 +148,10 @@ func (s *Session) startNetstackDataPlaneLoop() {
 				spi = binary.BigEndian.Uint32(espData[0:4])
 			}
 
-			s.Logger.Debug(s.pfx("netstack ESP 入站收到"),
-				logger.Uint64("espRecvCount", espRecvCount),
-				logger.Uint32("spi", spi),
-				logger.Int("len", len(espData)))
+			// s.Logger.Debug(s.pfx("netstack ESP 入站收到"),
+			// 	logger.Uint64("espRecvCount", espRecvCount),
+			// 	logger.Uint32("spi", spi),
+			// 	logger.Int("len", len(espData)))
 
 			sa := s.ChildSAIn
 			if len(espData) >= 4 && s.ChildSAsIn != nil {
@@ -178,10 +178,10 @@ func (s *Session) startNetstackDataPlaneLoop() {
 				return
 			case s.innerRx <- cp:
 				rxCount++
-				s.Logger.Debug(s.pfx("netstack ESP 入站已解密"),
-					logger.Uint64("rxCount", rxCount),
-					logger.Uint32("spi", spi),
-					logger.Int("plainLen", len(packet)))
+				// s.Logger.Debug(s.pfx("netstack ESP 入站已解密"),
+				// 	logger.Uint64("rxCount", rxCount),
+				// 	logger.Uint32("spi", spi),
+				// 	logger.Int("plainLen", len(packet)))
 			default:
 				s.Logger.Warn(s.pfx("netstack 入站队列已满，丢弃数据包"), logger.Int("len", len(packet)))
 			}
